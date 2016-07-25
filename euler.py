1b1121818f689f0e3a12cc8db18a8404299184eb
Michael A. Perlin <mika.perlin@gmail.com>
2016-07-24 23:32:55 -0400

    make euler.py print how many puzzles were solved

--- euler.py
+++ euler.py
@@ -3,12 +3,15 @@
 import tempfile
 import subprocess as sp
 
+puzzle_count = 0
+solved_puzzles = 0
 with open("puzzles.txt",'r') as f:
 
     for line in f:
 
         if "Grid" in line:
             puzzle = ""
+            puzzle_count += 1
             print(line.strip())
             continue
         else: puzzle += line.strip()
@@ -23,3 +26,7 @@ with open("puzzles.txt",'r') as f:
             output, _ = sp.Popen(cmd_list, stdout=sp.PIPE).communicate()
             output = output.decode("utf-8").strip()
             print(output)
+
+            if not "not solved" in output: solved_puzzles += 1
+
+print("solved: {}/{}".format(solved_puzzles,puzzle_count))
