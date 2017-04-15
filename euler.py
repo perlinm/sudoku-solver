d178f155cb3e2850a08bb83215d7b528087309ed
Michael A. Perlin <mika.perlin@gmail.com>
2017-04-15 00:56:20 -0600

    chaned euler.py to use different puzzle files

--- euler.py
+++ euler.py
@@ -1,34 +1,40 @@
 #!/usr/bin/env python3
 
-import tempfile
+import sys, os, tempfile
 import subprocess as sp
+
+if len(sys.argv) != 2:
+    print("usage: {} [puzzle file]".format(sys.argv[0]))
+    exit(1)
+
+master_file = sys.argv[1]
+if not os.path.isfile(master_file):
+    print("the provided file does not exist")
+    exit(1)
 
 puzzle_count = 0
 solved_puzzles = 0
-with open("puzzles.txt",'r') as f:
+with open(master_file, "r") as f:
 
     for line in f:
+        puzzle = line.strip()
+        if len(puzzle) != 81: continue
+        puzzle_count += 1
 
-        if "Grid" in line:
-            puzzle = ""
-            puzzle_count += 1
-            continue
-        else: puzzle += line.strip()
+        puzzle_file = tempfile.NamedTemporaryFile('w',delete=False)
+        puzzle_file.write(puzzle+"\n")
+        puzzle_file.close()
+        cmd_list = ["./solve-sudoku.exe",
+                    "--puzzle", puzzle_file.name]
+        output, _ = sp.Popen(cmd_list, stdout=sp.PIPE).communicate()
+        output = output.decode("utf-8").strip()
 
-        if len(puzzle) == 81:
-            puzzle_file = tempfile.NamedTemporaryFile('w',delete=False)
-            puzzle_file.write(puzzle+"\n")
-            puzzle_file.close()
-            cmd_list = ["./solve-sudoku.exe",
-                        "--puzzle", puzzle_file.name]
-            output, _ = sp.Popen(cmd_list, stdout=sp.PIPE).communicate()
-            output = output.decode("utf-8").strip()
-
-            if "not solved" in output:
-                print("\npuzzle {}:".format(puzzle_count))
-                print(" ".join(cmd_list))
-                print(output)
-            else:
-                solved_puzzles += 1
+        if "not solved" in output:
+            print("puzzle {}:".format(puzzle_count))
+            print(" ".join(cmd_list))
+            print(output)
+            print()
+        else:
+            solved_puzzles += 1
 
 print("solved: {}/{}".format(solved_puzzles,puzzle_count))
